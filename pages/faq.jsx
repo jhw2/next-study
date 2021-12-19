6e186831c7083050f8b5c998439c1706fc5d90bf
hope5107@naver.com <hope5107@naver.com>
2021-12-20 08:02:10 +0900

    구글로그인

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { db } from '../firebase'; 
 import { collection, addDoc, getDocs } from "firebase/firestore";
-import { async } from '@firebase/util';
+import { getAuth, signInWithPopup, GoogleAuthProvider  } from "firebase/auth";
 
 export default function faq() {
     const formRef = useRef();
@@ -34,6 +34,34 @@ export default function faq() {
     }
 
     useEffect(async()=>{
+        const auth = getAuth();
+        const user = auth.currentUser;
+        const provider = new GoogleAuthProvider();
+        
+        console.log(user)
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access Google APIs.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user)
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(errorMessage)
+            // ...
+        });
+
+
+
         setFaqList(await getData());
     },[]);
 
